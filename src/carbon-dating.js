2411abc3ce69f9e0606c7702439f462257032bc0
Мария Светлая <merry76light@gmail.com>
2021-03-29 01:15:07 +0300

    carbon

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,23 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
+module.exports = function dateSample(sampleActivity) {
   throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  let t, k, m, res;
+    if (sampleActivity === ""){
+      res = false;
+    }
+    else if (sampleActivity && typeof sampleActivity == 'string'){
+      k = Math.log(2)/HALF_LIFE_PERIOD;
+      m = Math.log(MODERN_ACTIVITY/sampleActivity);
+      if (isNaN(m) == true || m == Infinity){
+        res = false;
+      }
+      else {
+          t = Math.ceil(m/k);
+          (t>0) ? res = t : res = false;
+      }
+    }
+    else res = false;
+    return res;
 };
