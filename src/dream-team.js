d7053b31fbd90cc5137ab297fd8202af42d51abc
Мария Светлая <merry76light@gmail.com>
2021-03-29 09:30:48 +0300

    feat: add deream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+ let arr = [];
+ let str;
+ if (Array.isArray(members) == false){
+    str = false;
+  } 
+else {
+   for (let items of members){
+    if  (typeof items !== "string") { 
+      str = false
+    }
+    else {
+      items = items.trim();
+      arr.push(items[0].toUpperCase());
+    }
+  }
+  arr.sort();
+  console.log("arr = " + arr);
+  str = arr.join("");
+ 
+}
+console.log("str = " + str);
+ return str;
 };
