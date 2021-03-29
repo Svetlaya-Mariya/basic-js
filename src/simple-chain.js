3e7c78f84a6225e238ce28bb841d5dd22b7c1965
Мария Светлая <merry76light@gmail.com>
2021-03-29 19:10:28 +0300

    feat: chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,19 +1,21 @@
 const CustomError = require("../extensions/custom-error");
  const chainMaker = {
-  arr : 0,
+  arr : [],
   getLength() {
-    let len = arr.length;
-   return len;
+   return this.arr.length;
   },
   addLink(value) {
-    /*if (value === undefined){
-     // throw new Error();
-    }*/
-    this.arr.push("(" + value + ")~~");
-    return this;
+    if (value === undefined){
+      throw new Error();
+    }
+    else this.arr.push(value);
+  return this;
   },
   removeLink(position) {
-    this.arr.splice(position-1, 1);
+    if (this.arr[position - 1] === undefined || typeof position !== "number"){
+      throw new Error();
+    }
+    else this.arr.splice(position-1, 1);
     return this;
   },
   reverseChain() {
@@ -21,9 +23,18 @@ const CustomError = require("../extensions/custom-error");
    return this;
   },
   finishChain() {
-    return this;
+    let result = this.arr.reduce((acc, el, i) => {
+      if (i === 0) {
+        acc += `( ${el} )`;
+        return acc;
+      } else {
+        acc += `~~( ${el} )`;
+        return acc;
+      }
+    }, "");
+    this.arr = [];
+    return result;
   }
 };
-console.log(arr);
-
+console.log(chainMaker);
 module.exports = chainMaker;
