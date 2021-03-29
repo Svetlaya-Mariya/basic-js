ecee13eb7744d872d9bb1130c32cb0442cb7c013
Мария Светлая <merry76light@gmail.com>
2021-03-29 16:44:58 +0300

    feat: transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let newArr = [];
+  if (arr && Array.isArray(arr)){
+    for (let i=0; i<arr.length; i++){
+      if (arr[i] ==="--double-next") {
+        if (arr[i+1] != null){
+          newArr.push(arr[i+1])
+        }
+      }
+      else if (arr[i] === "--double-prev"){
+        if (arr[i-1] != null){
+          newArr.push(arr[i-1])
+        }
+        else continue;
+      }
+      else if (arr[i] === "--discard-next"){
+        if (arr[i+1] != null){
+          i = i+2;
+        }
+      }
+      else if (arr[i] ==="--discard-prev") {
+        if (arr[i-1] != null && arr[i-2] != "--discard-next") {
+          newArr.pop();
+        }
+      }  
+     else newArr.push(arr[i]);
+    }
+    console.log("arr = " + arr);
+    console.log("new = " + newArr);
+  }
+  else throw new Error();
+/*console.log(arr);
+console.log(newArr);*/
+  return newArr;
 };
