29c7d407ac81f54c9ecfba2dc0ca4d64dbc92823
DESKTOP-LKHU9F7\merry <merry76light@gmail.com>
2023-04-16 23:56:05 +0300

    feat: add hanoi tower and transform array 04/16/2023 23:56:05

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  let newArr = [];
+  if (arr && Array.isArray(arr) && arr instanceof Array){
+    for (let i=0; i<arr.length; i++){
+      if (arr[i] ==="--double-next") {
+        if (arr[i+1] != null){
+          newArr.push(arr[i+1])
+        }
+      }
+      else if (arr[i] === "--double-prev"){
+        if (arr[i-1] != null){
+          newArr.push(arr[i-1])
+        }
+        else continue;
+      }
+      else if (arr[i] === "--discard-next"){
+        if (arr[i+1] != null){
+          i = i+2;
+        }
+      }
+      else if (arr[i] ==="--discard-prev") {
+        if (arr[i-1] != null && arr[i-2] != "--discard-next") {
+          newArr.pop();
+        }
+      }  
+     else newArr.push(arr[i]);
+    }
+  }
+  else throw new Error('\'arr\' parameter must be an instance of the Array!');
+  return newArr;
 }
 
 module.exports = {
