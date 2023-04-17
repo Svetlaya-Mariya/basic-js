e5baa54f6ecaa84a60fe32027007b8f3801782bd
DESKTOP-LKHU9F7\merry <merry76light@gmail.com>
2023-04-17 17:42:58 +0300

    feat: add extended-repeater04/17/2023 17:42:58

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  if (!options.separator){
+    options.separator = '+'
+  }
+  if (options.addition === undefined){
+    options.addition = '';
+  }
+  if(!options.repeatTimes){
+    options.repeatTimes = 1
+  }
+  if(!options.additionRepeatTimes){
+    options.additionRepeatTimes = 1
+  }
+  if(!options.additionSeparator){
+    options.additionSeparator = '|'
+  }
+  if (typeof str !== 'string'){
+    str=String(str);
+  }
+  if (typeof options.addition !== 'string'){
+    options.addition=String(options.addition);
+  }
+  const array = Array(options.repeatTimes).fill(str + Array(options.additionRepeatTimes).fill(options.addition).join(options.additionSeparator));
+  return array.join(options.separator);
 }
 
 module.exports = {
